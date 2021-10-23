db5c7bd898b148e6b46e8b862a55c3945def2db0
chaogang <chaogang.chen@aliyun.com>
2021-10-23 23:42:56 +0800

    stupid realize of lengthOfLongestSubstring question

--- 无重复最长字符串.cpp
+++ 无重复最长字符串.cpp
@@ -5,9 +5,47 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        if(s.length()==0)return 0;//!!!!!!!!!!!!!!!!!!!!!!!用于判定空字符串
+        int flag = 0;
+        int start = 0;
+        int end = 0;
+        for(int i=0;i<s.length();i++){
+            
+            string table = "";
+            for(int j=i;j<s.length();j++){
+                if(table.find(s[j])==string::npos){
+                    table=table+s[j];
 
+                }else{
+                    table = "";
+                    if(j-i > end-start){start = i; end=j;}
+                    //cout<<"第"<<i<<"次，start="<<start<<"end="<<end<<endl;
+                    flag = 1;
+                    break;//！！！！！！！！！！！！！这里的break起到退出函数的作用
+                }
+            }
+        }
+        string table=""; //！！！！！！！！！！！这里避免了变量局部作用于造成的bug
+        //!!!!!!!!!!!!!!!!!下面这一段用于识别出现在末尾的连续字符串
+        for(int i=s.length();i>=0;i--){
+            if(table.find(s[i])==string::npos){
+                    table=table+s[i];
+            }else{
+                break;
+            }
+        }
+        if(table.length()-1>end-start) return table.length()-1;
+        if(flag)return (end-start);
+        else return s.length();
     }
 };
+
+int main(){
+    Solution a;
+    int res = a.lengthOfLongestSubstring("cdd");
+    cout << res<<endl;
+    return 0;
+}
 
 /*
 思路1:暴力
@@ -29,7 +67,7 @@ return s[i:j] 返回从i到j的闭区间
 */
 
 /*
-思路二：递归
+思路二：递归 ！！！！！！！！可行性有待商榷
 int find(string s, int start=0, int end=len(s)-1){
 基态 
 if start==end: 
@@ -46,3 +84,4 @@ int find_cross(string s, int start, int end){
     mid = start + (end-start)>>1
 }
 */
+
