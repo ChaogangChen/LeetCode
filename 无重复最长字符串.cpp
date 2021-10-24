73980644dc31580d5cb8ecbe4be80c4b77593b51
chaogang <chaogang.chen@aliyun.com>
2021-10-24 19:13:11 +0800

    window algotithm using unordered_set<char>

--- 无重复最长字符串.cpp
+++ 无重复最长字符串.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
+#include <unordered_set>
 using namespace std;
 
+//解法一：暴力算法，复杂度n^2
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -37,6 +40,25 @@ public:
         if(table.length()-1>end-start) return table.length()-1;
         if(flag)return (end-start);
         else return s.length();
+    }
+};
+
+//解法2，窗口算法，复杂度O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left = 0;
+        int maxSize = 0;
+        unordered_set<char> lookup;
+        for(int i=0; i<s.length(); i++){
+            while(lookup.find(s[i]) != lookup.end()){ //当find没找到元素的时候返回end，找到的时候返回元素的位置
+                lookup.erase(s[left]); //!!!!!!!!!!!!!!!这里需要删掉最左边的元素才能实现移动窗口的目的，如果错删成s[i]会造成执行bug
+                left = left+1;
+            }
+            maxSize = max(maxSize,i-left+1);
+            lookup.insert(s[i]);
+        }
+        return maxSize;
     }
 };
 
@@ -85,3 +107,35 @@ int find_cross(string s, int start, int end){
 }
 */
 
+/*
+思路三：滑动窗口，这种算法的时间复杂度为O(n)
+创建初始窗口
+start = 0
+end = 0
+while(end<=s.length()){
+    if 不重复s[end] in s[start:end+1]：
+        扩列 end = end+1
+        continue
+    else if 重复s[end] not in s[start:end+1]:
+        移动队列 start = start+1;end = end+1
+        continue
+    end = end+1;
+}
+!!!!!!!!!!!!!!!!
+答案中的另一种循环
+left = 0;
+maxSize = 0;
+tmp = [];
+for(int i =0;i<s.length();i++){
+    while s[i] in tmp:
+        from tmp delete s[i]
+        left ++
+    tmp = tmp+s[i]
+    maxSize = max(maxSize,i-left+1)
+return maxSize
+}
+
+！！！！！！！！！
+
+return end-start
+*/
